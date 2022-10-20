4fba4b6937ab565e161468441aa4351b54396918
eremophobia <succducc0@gmail.com>
2022-10-20 15:16:46 +0200

    + pDL

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from wget import download
 from posixpath import basename
 from urllib import parse
+import urllib.request
+from time import sleep
 import sys
 
-URL = input('File URL: ')
+URL = sys.argv[1]
 
 path = parse.urlsplit(URL).path
 domain = parse.urlparse(URL).netloc
 filename = basename(path)
+filesize = urllib.request.urlopen(URL)
 
-print(f"\nDownloading... | {filename} | From - {domain}")
+# Function to convert Bytes from file to Mb, Gb, etc . . .
+def format_bytes(size):
+    # 2**10 = 1024
+    power = 2**10
+    n = 0
+    power_labels = {0 : '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return size, power_labels[n]+'B'
+
+print(f"\nDownloading... | {filename} | Size - {format_bytes(filesize.length)} | From - {domain}")
 response = download(URL, filename)
-input("\n\nDownload complete! Press Enter to close this window.\n")
+print("\n\nDownload complete! The program will close soon.")
 
+sleep(5)
 sys.exit()
