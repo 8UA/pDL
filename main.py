75b6275b2e2f84eae5600af8c0147504b5845b79
eremophobia <succducc0@gmail.com>
2022-10-29 00:32:17 +0200

    + pDL | fixed imports

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 try:
     from wget import download
+    import urllib, urllib.parse, urllib.request, urllib.error
     from posixpath import basename
-    from urllib import parse
-    import urllib.request
     from time import sleep
     import sys
 except ModuleNotFoundError:
     import subprocess
-    print("\nModules are missing, installing requirements. (Make sure your wifi is activated)")
+    print("\nModules are missing, installing requirements. (Make sure your wifi is connected)")
     subprocess.call("pip install -r requirements.txt", shell=True)
     print("Requirements installed, restart the program.")
     exit()
@@ -17,7 +16,7 @@ URL = sys.argv[1]
 # Fetching file name, file size and destination domain . . .
 filename = basename(URL)
 filesize = urllib.request.urlopen(URL)
-domain = parse.urlparse(URL).netloc
+domain = urllib.parse.urlparse(URL).netloc
 
 # Function to convert Bytes from file to MB, GB, etc . . .
 def format_bytes(size):
@@ -30,10 +29,9 @@ def format_bytes(size):
         n += 1
     return size, power_labels[n]+'B'
 
-# Start file download . . .
-print(f"\nDownloading... | {filename} | Size - {format_bytes(filesize.length)} | From - {domain}")
-
+# Print file info & start download . . .
 try:
+    print(f"\nDownloading... | {filename} | Size - {format_bytes(filesize.length)} | From - {domain}")
     response = download(URL, filename)
 except KeyboardInterrupt:
     print("\n\nKeyboardInterrupt detected, download stopped.")
@@ -41,6 +39,5 @@ except KeyboardInterrupt:
     sys.exit()
 
 print("\n\nDownload complete! The program will close soon.")
-
 sleep(5)
 sys.exit()
